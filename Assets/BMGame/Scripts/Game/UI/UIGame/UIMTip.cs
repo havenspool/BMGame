abffba2165164db0c497e846f60f4915e641a6cf
watkin <jski.chen@gmail.com>
2015-11-04 18:52:16 +0800

    add Tip UI Asset

--- Assets/BMGame/Scripts/Game/UI/UIGame/UIMTip.cs
+++ Assets/BMGame/Scripts/Game/UI/UIGame/UIMTip.cs
@@ -2,7 +2,17 @@
 using System.Collections;
 
 public class UIMTip : MonoBehaviour {
-	private UMovie sprite;
 
+	private UMovie tMovie;
+	private UMovie runMovie;
+	private UMovie yesMovie;
+	private UMovie noMovie;
+
+	void Awake(){
+		tMovie = transform.Find("TMovie").GetComponent<UMovie>();
+		runMovie = transform.Find("RunMovie").GetComponent<UMovie>();
+		yesMovie = transform.Find("YesMovie").GetComponent<UMovie>();
+		noMovie= transform.Find("NoMovie").GetComponent<UMovie>();
+	}
 
 }
